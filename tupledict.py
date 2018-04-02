62597be0af9f18f19bf31d7c7ea540e04f1546fb
Merianitbn06 <36938522+Merianitbn06@users.noreply.github.com>
2018-04-02 17:18:24 +0700

    Add files via upload

---
+++ tupledict.py
@@ -0,0 +1,20 @@
+tupA=(1,2,3,4)
+print tupA
+
+tupB=(5,6,7,8)
+print tupB
+
+tupC=tupA+tupB
+print tupC
+
+tupD=tupC*2
+print tupD
+
+print tupD[3]
+
+print tupD[-3:]
+
+print len(tupD)
+
+dict1={}
+dict1 
